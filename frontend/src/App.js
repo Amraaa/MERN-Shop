6ceff60db375621715b22cd70311a41423ffc666
Amraaa <amratopuz1@gmail.com>
2020-12-15 20:05:37 +0100

    second commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react'
-
+import Header from './components/Header'
+import Footer from './components/Footer'
 const App = () => {
   return (
     <>
-      <h1>Welcome To MERNShop</h1>
+      <Header />
+      <maim>
+        <h1>Welcome To MERNShop</h1>
+      </maim>
+      <Footer />
     </>
   )
 }
