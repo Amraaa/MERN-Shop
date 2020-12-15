124209389f40deddeb7b380a2889d5b42587bd86
Amraaa <amratopuz1@gmail.com>
2020-12-15 22:40:57 +0100

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
 const App = () => {
   return (
     <>
       <Header />
-      <maim>
-        <h1>Welcome To MERNShop</h1>
-      </maim>
+      <main>
+        <Container>
+          <h1>Welcome To MERNShop</h1>
+        </Container>
+      </main>
       <Footer />
     </>
   )
